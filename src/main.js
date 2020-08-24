ad4881899a4193409330b814bd517f3f5508a583
Matias Apolonio <matias.apolonio@autodesk.com>
2020-08-23 20:00:19 -0400

    added analytics

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue';
+import VueAnalytics from 'vue-analytics';
 import App from './App.vue';
 
 Vue.config.productionTip = false;
+Vue.use(VueAnalytics, {
+  id: 'UA-176058486-1'
+});
 
 new Vue({
   render: h => h(App)
